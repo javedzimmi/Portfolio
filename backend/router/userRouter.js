4dd24deb67c270fdf72f721e6e3122756e5e2a4a
javed alam <javedalamvvvvv2004@gmail.com>
2025-05-28 13:31:37 +0530

    changes

--- backend/router/userRouter.js
+++
@@ -1,9 +0,0 @@
-// routes/userRoutes.js
-import express from "express";
-import userdata from "../controller/userControler.js";
-
-const userRouter = express.Router();
-
-userRouter.post("/user", userdata);
-
-export default userRouter;
